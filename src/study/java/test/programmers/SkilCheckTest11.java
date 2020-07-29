4381e811556f654e021b76ae7e00348f7ec2873c
김두진 <djkorea@sk.com>
2020-07-29 19:27:06 +0900

    level 1 done

---
+++ src/study/java/test/programmers/SkilCheckTest11.java
@@ -0,0 +1,42 @@
+package study.java.test.programmers;
+
+/*
+문제 설명
+String형 배열 seoul의 element중 Kim의 위치 x를 찾아, 김서방은 x에 있다는 String을 반환하는 함수, solution을 완성하세요. seoul에 Kim은 오직 한 번만 나타나며 잘못된 값이 입력되는 경우는 없습니다.
+
+제한 사항
+seoul은 길이 1 이상, 1000 이하인 배열입니다.
+seoul의 원소는 길이 1 이상, 20 이하인 문자열입니다.
+Kim은 반드시 seoul 안에 포함되어 있습니다.
+입출력 예
+seoul	return
+[Jane, Kim]	김서방은 1에 있다
+
+A
+채점 결과
+정확성: 50.0
+효율성: 0.0
+합계: 50.0 / 50
+ */
+public class SkilCheckTest11 {
+    public static void main(String[] args) {
+        SkilCheckTest11 test = new SkilCheckTest11();
+        String[] str = {"Jane", "Kim"};
+        System.out.println(test.solution(str));
+    }
+
+    public String solution(String[] seoul) {
+        int limit = seoul == null? 0 : seoul.length > 1000 ? 1000 : seoul.length;
+        String targetName = "Kim";
+        int location = 0;
+
+        for (int i = 0; i < limit; i++) {
+            if(targetName.equals(seoul[i])){
+              location = i;
+              break;
+            }
+        }
+        String answer = "김서방은 "+location+"에 있다";
+        return answer;
+    }
+}
---
+++ src/study/java/test/programmers/SkilCheckTest14.java
@@ -0,0 +1,44 @@
+package study.java.test.programmers;
+
+/*
+문제 설명
+두 정수 a, b가 주어졌을 때 a와 b 사이에 속한 모든 정수의 합을 리턴하는 함수, solution을 완성하세요.
+예를 들어 a = 3, b = 5인 경우, 3 + 4 + 5 = 12이므로 12를 리턴합니다.
+
+제한 조건
+a와 b가 같은 경우는 둘 중 아무 수나 리턴하세요.
+a와 b는 -10,000,000 이상 10,000,000 이하인 정수입니다.
+a와 b의 대소관계는 정해져있지 않습니다.
+입출력 예
+a	b	return
+3	5	12
+3	3	3
+5	3	12
+
+A
+채점 결과
+정확성: 50.0
+효율성: 0.0
+합계: 50.0 / 50
+
+ */
+public class SkilCheckTest14 {
+    public static void main(String[] args) {
+        SkilCheckTest14 test = new SkilCheckTest14();
+        System.out.println(test.solution(3, 3));
+
+    }
+
+
+    public long solution(int a, int b) {
+        long answer = 0;
+        int from = a > b ? b : a;
+        int to = b > a ? b : a;
+
+        for(int i=from;i<=to;i++){
+            answer += i;
+        }
+
+        return answer;
+    }
+}
